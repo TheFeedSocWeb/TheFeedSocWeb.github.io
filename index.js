f15bd9ca07890057a4a7e8c8fc88b15842ea00e9
TheFeedSocWeb <99533743+TheFeedSocWeb@users.noreply.github.com>
2022-02-13 15:06:55 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+//SideBar
+const menuItems = document.querySelectorAll('.menu-item');
+
+
+//remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.add('active');
+    })
+})
